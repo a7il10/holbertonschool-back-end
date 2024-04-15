9d29ebad1a76247d19b493edaa52a9ea3b1e940e
a7il10 <atilio.sulovari199@gmail.com>
2024-04-15 11:57:41 +0200

    update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,20 +1,20 @@
 import requests
 
-def get_employee_todo_progress(employee_id):
-    # Get the employee's details
+def todo_list_progress(employee_id):
+    # Get the employee data
     employee = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
-    # Get the employee's todos
+    # Get the TODOs for the employee
     todos = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos').json()
 
     # Calculate the progress
     total_tasks = len(todos)
-    done_tasks = len([task for task in todos if task['completed']])
-    
+    done_tasks = len([todo for todo in todos if todo['completed']])
+    done_tasks_titles = [todo['title'] for todo in todos if todo['completed']]
+
     # Print the progress
     print(f"Employee {employee['name']} is done with tasks({done_tasks}/{total_tasks}):")
-    for task in todos:
-        if task['completed']:
-            print(f"\t {task['title']}")
+    for title in done_tasks_titles:
+        print(f"\t {title}")
 
 # Test the function with an example employee ID
-get_employee_todo_progress(1)
+todo_list_progress(1)
